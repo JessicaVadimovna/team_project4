0bece7bae37ca7ed1308d32dad04194412b02149
Lina <linochka-klm@mail.ru>
2024-04-15 22:27:42 +0300

    new sass

--- js/header-links.js
+++ js/header-links.js
@@ -4,9 +4,9 @@ const hamburger= document.querySelector(".hamburger");
 // const closeIcon= document.querySelector(".closeIcon");
 // const menuIcon = document.querySelector(".menuIcon");
 
-function burgerMenu(icon) {
-   icon.classList.toggle("change");
- }
+// function burgerMenu(icon) {
+//    icon.classList.toggle("change");
+//  }
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
@@ -30,3 +30,39 @@ menuItems.forEach(
 )
 
 
+
+
+const linksDropdown1 = document.getElementById('linksDropdown1');
+const buttonLink1 = document.querySelector('.navLink1');
+const divDropdown = document.querySelector('links-dropdown');
+
+
+
+
+
+
+// ------------------------------NEWS---------------------------
+
+const newsOne = document.querySelector('.main-news__one');
+const newsTwo = document.querySelector('.main-news__two');
+const newsThree = document.querySelector('.main-news__three');
+const newsButtonOne = document.getElementById('newsButtonTwo');
+const newsButtonTwo = document.getElementById('newsButtonTwo');
+const newsButtonThree = document.getElementById('newsButtonThree');
+
+
+function newsApi() {
+  fetch("https://ok.surf/api/v1/news-feed", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: "no-cors",
+  })
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((error) => {
+    console.log(error);
+  })
+}
+newsApi();
