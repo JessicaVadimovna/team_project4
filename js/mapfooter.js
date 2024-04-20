b2b834cd1fbb3ae3be7617f219ff8b1518dc82e0
iameemilia <146082617+iameemilia@users.noreply.github.com>
2024-04-20 22:10:11 +0300

    footer

--- js/mapfooter.js
+++ js/mapfooter.js
@@ -1,27 +1,8 @@
 ymaps.ready(init);
 
 function init() {
-    var myMap = new ymaps.Map("map", {
-        center: [55.765546, 37.638180],
-        zoom: 13
-    });
-
-    var placemarks = [
-        {
-            coordinates: [55.755481, 37.606476],
-            title: 'Московская Биржа, Большой Кисловский пер., 13, Москва',
-        },
-        {
-            coordinates: [55.772046, 37.675127],
-            title: 'Московская Биржа, Спартаковская ул., 12, Москва',
-        }
-    ];
-
-    placemarks.forEach(function (placemark) {
-        var marker = new ymaps.Placemark(placemark.coordinates, {
-            hintContent: "Мосбиржа",
-            balloonContentHeader: placemark.title,
-        });
-        myMap.geoObjects.add(marker);
+    var myMap = new ymaps.Map('map', {
+        center: [55.76, 37.64],
+        zoom: 10
     });
 }
